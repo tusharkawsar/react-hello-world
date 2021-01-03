80adacd0292874c90939b765d50e595569815d90
Tushar Kawsar <tushar.kawsar@gmail.com>
2021-01-02 21:33:30 -0500

    added a React.createElement method inside the functional component

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -4,11 +4,24 @@ import React from "react";
 //     return <h1> Hello, world </h1>;
 // }
 
+// const Greet = () => {
+//     return (
+//         <div className="dummyClass">
+//         {/* NOT class, but className */}
+//             <h1> Hello, world </h1>
+//         </div>
+//     );
+// };
+
 const Greet = () => {
-    return (
-        <div>
-            <h1> Hello, world </h1>
-        </div>
+    return React.createElement(
+        "div",
+        { className: "dummyClass" },
+        React.createElement(
+            "h1",
+            null,
+            "Hello, world"
+        )
     );
 };
 
