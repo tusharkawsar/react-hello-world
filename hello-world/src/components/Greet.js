29a90d8abe670e1a6e597945d47c4eb3f8e63927
Tushar Kawsar <tushar.kawsar@gmail.com>
2021-01-02 21:29:00 -0500

    added an arrow function inside the functional component

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-function Greet() {
-    return <h1> Hello, world </h1>;
-}
+// function Greet() {
+//     return <h1> Hello, world </h1>;
+// }
+
+const Greet = () => {
+    return (
+        <div>
+            <h1> Hello, world </h1>
+        </div>
+    );
+};
 
 export default Greet;
