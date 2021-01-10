ea631cea9244f98c54c320f035ce35d6cd4da5ed
Tushar Kawsar <tushar.kawsar@gmail.com>
2021-01-10 07:52:48 -0500

    Modified FraagmentDemo

--- advanced-guide-demo/src/App.js
+++ advanced-guide-demo/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
         return (
             <div className="App">
                 <h1> Tushar </h1>
-                <FragmentDemo />;
+                <FragmentDemo />
             </div>
         );
     }
--- advanced-guide-demo/src/components/FragmentDemo.js
+++ advanced-guide-demo/src/components/FragmentDemo.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function FragmentDemo() {
-    return <div>Hello</div>;
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
 }
 
 export default FragmentDemo;
